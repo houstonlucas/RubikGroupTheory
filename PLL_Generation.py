3137c6a174043c03523dad04ab6ee70949f05c88
houstonlucas <houstonlucas@nevada.unr.edu>
2021-02-18 17:09:13 -0800

    Extracted the drawing functionality from the PLL_State into the PLL_State_Drawer

--- PLL_Generation.py
+++ PLL_Generation.py
@@ -1,6 +1,6 @@
 import random
 import time
-from typing import Tuple
+from typing import Tuple, List
 
 import numpy as np
 import pyglet
@@ -10,6 +10,7 @@ from pyglet.graphics import Batch
 from pyglet.gl import *
 
 WINDOW_WIDTH = 1500
+
 
 def main():
     window = SimWindow()
@@ -21,18 +22,31 @@ class SimWindow(pyglet.window.Window):
         super(SimWindow, self).__init__(WINDOW_WIDTH, WINDOW_WIDTH)
         self.sim_dt = 1.0 / 60.0
 
-        state_array = PLL_State.solved_PLL_state()
+        state_array = PLL_State.solved_pll_state()
         n = 25
-        P2_10 = create_permutation_matrix(2, 10, n)
+
         P10_14 = create_permutation_matrix(10, 14, n)
         P10_22 = create_permutation_matrix(10, 22, n)
         P = P10_14 @ P10_22
-        state_array = get_permuted_state(state_array, P10_22)
-        state_array = get_permuted_state(state_array, P10_14)
+        Pinv = P.T
 
-        state_array = get_permuted_state(state_array, P)
+        state_array2 = get_permuted_state(state_array, P)
+        state_array3 = get_permuted_state(state_array, Pinv)
 
-        self.pll_state = PLL_State(state_array, position=(self.width / 2.0, self.height / 2.0), width=self.width)
+        pll_state = PLL_State(state_array)
+        pll_state2 = PLL_State(state_array2)
+        pll_state3 = PLL_State(state_array3)
+
+        pos1 = tuple(map(float, (self.width/4.0, self.height/4.0)))
+        pos2 = tuple(map(float, (self.width*3/4.0, self.height/4.0)))
+        pos3 = tuple(map(float, (self.width/4.0, self.height*3/4.0)))
+        state_width = self.width/4
+
+        self.pll_state_drawer = PLL_State_Drawer()
+
+        self.pll_state_drawer.prepare_state(pll_state, pos1, state_width)
+        self.pll_state_drawer.prepare_state(pll_state2, pos2, state_width)
+        self.pll_state_drawer.prepare_state(pll_state3, pos3, state_width)
 
     def sim_start(self):
         glClearColor(0.5, 0.5, 0.5, 1.0)
@@ -45,10 +59,13 @@ class SimWindow(pyglet.window.Window):
 
     def render(self):
         self.clear()
-        self.pll_state.draw()
+        self.pll_state_drawer.draw()
 
 
 class PLL_State:
+    tiles: ndarray
+    position: Tuple[float]
+
     colors = {
         "yellow": (255, 255, 0),
         "red": (255, 0, 0),
@@ -59,49 +76,11 @@ class PLL_State:
     }
     color_names = list(colors.keys())
 
-    batch: Batch
-    tiles: ndarray
-    position: Tuple[float]
-
-    def __init__(self, tiles: np.ndarray, position: Tuple[float], width: float):
-        self.position = tuple(value - width / 2 for value in position)
-        self.width = width
-        self.batch = Batch()
+    def __init__(self, tiles: np.ndarray):
         self.tiles = tiles
-        r, c = self.tiles.shape
-        assert (r == c)
-        self.num_rows = r
-        self.rects = []
-
-        # Compute size of components
-        self.gap_portion = 0.2
-        self.gap_size = self.gap_portion * width / (self.num_rows + 1)
-        self.tile_portion = 1.0 - self.gap_portion
-        self.tile_size = self.tile_portion * width / self.num_rows
-
-        extremes = [0, self.num_rows - 1]
-
-        for row_idx in range(self.num_rows):
-            for col_idx in range(self.num_rows):
-                color_id = self.tiles[row_idx][col_idx]
-                color = PLL_State.colors[PLL_State.color_names[color_id]]
-                tile_x = self.position[0] + (col_idx + 1) * self.gap_size + col_idx * self.tile_size
-                tile_y = self.position[1] + self.width - ((row_idx + 1) * (self.gap_size + self.tile_size))
-
-                # Set batch to None for corners
-                batch = None if row_idx in extremes and col_idx in extremes else self.batch
-                rect = shapes.Rectangle(
-                    tile_x, tile_y,
-                    self.tile_size, self.tile_size,
-                    color=color, batch=batch
-                )
-                self.rects.append(rect)
-
-    def draw(self):
-        self.batch.draw()
 
     @staticmethod
-    def solved_PLL_state() -> ndarray:
+    def solved_pll_state() -> ndarray:
         state_array = np.zeros((5, 5), dtype=np.int)
         state_array[0, 1:4] = PLL_State.get_color_idx("green")
         state_array[-1, 1:4] = PLL_State.get_color_idx("blue")
@@ -112,6 +91,50 @@ class PLL_State:
     @staticmethod
     def get_color_idx(color_name: str) -> int:
         return PLL_State.color_names.index(color_name)
+
+
+class PLL_State_Drawer:
+    batch: Batch
+    rects: List[shapes.Rectangle]
+
+    def __init__(self):
+        self.batch = Batch()
+        self.rects = []
+
+    def prepare_state(self, pll_state: PLL_State, position: Tuple[float], width: float):
+
+        position = tuple(value - width / 2 for value in position)
+
+        r, c = pll_state.tiles.shape
+        assert (r == c)
+        num_rows = r
+
+        # Compute size of components
+        gap_portion = 0.2
+        gap_size = gap_portion * width / (num_rows + 1)
+        tile_portion = 1.0 - gap_portion
+        tile_size = tile_portion * width / num_rows
+
+        extremes = [0, num_rows - 1]
+
+        for row_idx in range(num_rows):
+            for col_idx in range(num_rows):
+                color_id = pll_state.tiles[row_idx][col_idx]
+                color = PLL_State.colors[PLL_State.color_names[color_id]]
+                tile_x = position[0] + (col_idx + 1) * gap_size + col_idx * tile_size
+                tile_y = position[1] + width - ((row_idx + 1) * (gap_size + tile_size))
+
+                # Set batch to None for corners
+                batch = None if row_idx in extremes and col_idx in extremes else self.batch
+                rect = shapes.Rectangle(
+                    tile_x, tile_y,
+                    tile_size, tile_size,
+                    color=color, batch=batch
+                )
+                self.rects.append(rect)
+
+    def draw(self):
+        self.batch.draw()
 
 
 def create_permutation_matrix(i: int, j: int, n: int) -> ndarray:
